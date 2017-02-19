78a56e40a915a2da159ae98de53880b26142f805
Kelly Selden <kellyselden@gmail.com>
2017-02-18 20:22:08 -0800

    vr button

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 import raw from 'ember-macro-helpers/raw';
 import { eq } from 'ember-awesome-macros';
 
+const linksY = 4.5;
+const hoverRange = 0.25;
+const linksHoverTop = linksY + hoverRange;
+const linksHoverBottom = linksY - hoverRange;
+
 export default Ember.Controller.extend({
   queryParams: [
     'rotX',
@@ -10,6 +15,9 @@ export default Ember.Controller.extend({
 
   rotX: 0,
   rotY: 0,
+
+  linksHoverTop,
+  linksHoverBottom,
 
   shouldHideBackButton: eq('currentRouteName', raw('index'))
 });
