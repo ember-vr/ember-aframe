6043fe103174d363af0c73cb659fe2e810126d19
kellyselden <kellyselden@gmail.com>
2017-02-24 18:59:56 -0800

    use send helper

--- addon/helpers/emit.js
+++
@@ -1,7 +0,0 @@
-import { helper } from 'ember-helper';
-
-export default helper(([target, action, ...params]) => {
-  return () => {
-    target.send(action, ...params);
-  };
-});
--- app/helpers/emit.js
+++
@@ -1,1 +0,0 @@
-export { default, emit } from 'ember-a-frame/helpers/emit';
