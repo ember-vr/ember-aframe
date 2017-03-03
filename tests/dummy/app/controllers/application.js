15566244a1383239e1213ffacab9adf68658813f
Kelly Selden <kellyselden@gmail.com>
2017-03-02 23:47:39 -0800

    rootURL fix

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -4,10 +4,13 @@ import { readOnly } from 'ember-computed';
 import observer from 'ember-metal/observer';
 import raw from 'ember-macro-helpers/raw';
 import { eq } from 'ember-awesome-macros';
+import ENV from '../config/environment';
 
 // from https://github.com/aframevr/aframe/blob/master/src/constants/index.js#L3
 // https://github.com/aframevr/aframe/pull/2418
 // const DEFAULT_CAMERA_HEIGHT = 1.6;
+
+const { rootURL } = ENV;
 
 export default Controller.extend({
   people: injectService(),
@@ -25,6 +28,8 @@ export default Controller.extend({
   posX: 0,
   posY: 0,
   posZ: 0,
+
+  rootURL,
 
   shouldHideBackButton: eq('currentRouteName', raw('index')),
 
