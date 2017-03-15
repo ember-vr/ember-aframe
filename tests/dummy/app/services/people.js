47216eb0f7a14a6c076ed6c02b969a46e6b80a98
Kelly Selden <kellyselden@gmail.com>
2017-03-14 20:18:36 -0700

    socket leaving and reentering vr

--- tests/dummy/app/services/people.js
+++ tests/dummy/app/services/people.js
@@ -4,6 +4,91 @@ import { task, timeout } from 'ember-concurrency';
 export default Ember.Service.extend(Ember.Evented, {
   otherPeople: Ember.A(),
 
+  setUpSocket() {
+    var wsUri = "wss://ember-a-frame-server.herokuapp.com/";
+    let websocket = new WebSocket(wsUri);
+    websocket.onclose = function(evt) { onClose(evt) };
+    websocket.onmessage = function(evt) { onMessage(evt) };
+    websocket.onerror = function(evt) { onError(evt) };
+
+    let people = this;
+
+    people.set('ws', websocket);
+
+    function onClose(evt)
+    {
+      // debugger;
+    }
+
+    function onMessage(evt)
+    {
+      // websocket.close();
+      let data = JSON.parse(evt.data);
+      let id = data.id;
+      let otherPeople = people.get('otherPeople');
+      let person = otherPeople.findBy('id', id);
+      switch (data.type) {
+        // case 'id':
+        //   otherPeople.pushObject(Ember.Object.create({
+        //     id
+        //   }));
+        //   // everyone tells the new person their route
+        //   people.updateRoute();
+        //   break;
+        case 'move':
+          // if (person) {
+            person.set('params', data.data);
+          // }
+          break;
+        // case 'route':
+        //   if (data.data !== people.get('myRoute')) {
+        //     // you clear out others when you switch routes for instant effect
+        //     // then the propagation hits later so ignore it
+        //     // if (person) {
+        //       // another person switched routes
+        //       otherPeople.removeObject(person);
+        //     // }
+        //   } else {
+        //     // if (!person) {
+        //       otherPeople.pushObject(Ember.Object.create({
+        //         id
+        //       }));
+        //     // }
+        //   }
+        //   break;
+        case 'join':
+          // if (!person) {
+            otherPeople.pushObject(Ember.Object.create({
+              id
+            }));
+          // }
+          break;
+        case 'left':
+          // if (person) {
+            otherPeople.removeObject(person);
+          // }
+          break;
+      }
+    }
+
+    function onError(evt)
+    {
+      // debugger;
+    }
+
+    return new Ember.RSVP.Promise(resolve => {
+        websocket.onopen = function(evt) { onOpen(evt) };
+        function onOpen(evt)
+        {
+          // websocket.send(JSON.stringify({
+          //   type: 'connected'
+          // }));
+          // people.updateRoute();
+          resolve(websocket);
+        }
+    });
+  },
+
   updateLocation(params) {
     this.send({
       type: 'move',
@@ -12,6 +97,13 @@ export default Ember.Service.extend(Ember.Evented, {
   },
   updateRoute(route) {
     if (route) {
+      if (route.indexOf('vr.') !== 0) {
+        let ws = this.get('ws');
+        if (ws) {
+          ws.close();
+          this.set('ws', null);
+        }
+      }
       this.set('myRoute', route);
       this.get('otherPeople').clear();
     } else {
@@ -28,10 +120,18 @@ export default Ember.Service.extend(Ember.Evented, {
   },
 
   send(json) {
+    let promise;
     let ws = this.get('ws');
-    if (ws.readyState === WebSocket.OPEN) {
-      ws.send(JSON.stringify(json));
+    if (!ws) {
+      promise = this.setUpSocket();
+    } else {
+      promise = Ember.RSVP.resolve(ws);
     }
+    promise.then(ws => {
+      if (ws.readyState === WebSocket.OPEN) {
+        ws.send(JSON.stringify(json));
+      }
+    });
   },
 
   updateRouteTask: task(function * () {
