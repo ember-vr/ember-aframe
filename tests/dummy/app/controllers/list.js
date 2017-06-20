869cf3f1518e1d16352d4b9e788b6f0b9b341b64
Kelly Selden <kellyselden@gmail.com>
2017-06-20 15:25:10 -0700

    use ember-controller-lifecycle

--- tests/dummy/app/controllers/list.js
+++ tests/dummy/app/controllers/list.js
@@ -1,4 +1,31 @@
 import Controller from 'ember-controller';
+import EmberObject from 'ember-object';
+import get from 'ember-metal/get';
+import { defaultComponents } from 'ember-aframe/utils/components';
+import { defaultAttributes } from 'ember-aframe/utils/attributes';
+
+const modulePrefix = 'ember-aframe/components/';
 
 export default Controller.extend({
+  setup() {
+    let componentModules = Object.keys(requirejs._eak_seen).filter(module => {
+      return module.indexOf(`${modulePrefix}a-`) === 0;
+    });
+
+    this.set('components', componentModules.map(componentModule => {
+      let componentDefinition = requirejs(componentModule).default;
+      let component = componentDefinition.create({
+        renderer: {}
+      });
+      let attributeBindings = get(component, 'attributeBindings');
+      let nonDefaultAttributeBindings = attributeBindings.filter(attribute => {
+        return !defaultComponents.includes(attribute) && !defaultAttributes.includes(attribute);
+      });
+      return EmberObject.create({
+        tagName: componentModule.substr(modulePrefix.length),
+        nonDefaultAttributeBindings,
+        shouldHideToggle: !nonDefaultAttributeBindings.length
+      });
+    }));
+  }
 });
