ff4a42af6c9ef0680963ee3a4ff2dda89e1ff181
Kelly Selden <kellyselden@gmail.com>
2017-06-20 00:04:04 -0700

    remove rootURL

--- tests/dummy/app/controllers/vr.js
+++ tests/dummy/app/controllers/vr.js
@@ -7,13 +7,10 @@ import observer from 'ember-metal/observer';
 import get from 'ember-metal/get';
 import raw from 'ember-macro-helpers/raw';
 import { eq } from 'ember-awesome-macros';
-import ENV from '../config/environment';
 
 // from https://github.com/aframevr/aframe/blob/master/src/constants/index.js#L3
 // https://github.com/aframevr/aframe/pull/2418
 // const DEFAULT_CAMERA_HEIGHT = 1.6;
-
-const { rootURL } = ENV;
 
 export default Controller.extend({
   people: injectService(),
@@ -32,8 +29,6 @@ export default Controller.extend({
   posX: 0,
   posY: 0,
   posZ: 0,
-
-  rootURL,
 
   currentRouteName: readOnly('application.currentRouteName'),
 
