7fbcd695c3d88288e30c38aff40645687ecc70fc
Kelly Selden <kellyselden@gmail.com>
2019-11-05 15:22:31 +0000

    fix mouse over logic

--- tests/dummy/app/components/a-link.js
+++ tests/dummy/app/components/a-link.js
@@ -7,17 +7,13 @@ export default AEntity.extend({
 
   classNames: ['link'],
   attributeBindings: [
-    'event-set__1',
-    'event-set__2',
-    'event-set__3',
-    'event-set__4',
+    'event-set__mouseenter',
+    'event-set__mouseleave'
   ],
 
   geometry: 'primitive: plane; height: 1; width: 1',
   material: tag`shader: flat; src: ${'thumb'}`,
   sound: 'on: click; src: #click-sound',
-  'event-set__1': '_event: mousedown; scale: 1 1 1',
-  'event-set__2': '_event: mouseup; scale: 1.2 1.2 1',
-  'event-set__3': '_event: mouseenter; scale: 1.2 1.2 1',
-  'event-set__4': '_event: mouseleave; scale: 1 1 1'
+  'event-set__mouseenter': 'scale: 1.2 1.2 1',
+  'event-set__mouseleave': 'scale: 1 1 1'
 });
