4a5db98940f8a6a70ed6e3a29a1b5267da502eea
Kelly Selden <kellyselden@gmail.com>
2019-11-05 14:27:57 +0000

    fix for newer ember
    https://github.com/emberjs/ember.js/issues/17545

--- tests/dummy/app/routes/vr/360-image-gallery/-base.js
+++ tests/dummy/app/routes/vr/360-image-gallery/-base.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   beforeModel(transition) {
-    transition.send('updateSrc', this.get('updateSrc'));
+    transition.send(false, 'updateSrc', this.get('updateSrc'));
   }
 });
