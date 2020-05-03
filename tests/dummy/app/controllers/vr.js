cc612055274bd9f7c3b43e96f5aa0f9efb09d75f
Kelly Selden <kellyselden@gmail.com>
2020-05-03 18:43:33 +0100

    fix new lint errors

--- tests/dummy/app/controllers/vr.js
+++ tests/dummy/app/controllers/vr.js
@@ -1,4 +1,5 @@
 import Controller from '@ember/controller';
+// eslint-disable-next-line ember/no-mixins
 import QueryParamsControllerMixin from 'ember-aframe-camera-extras/mixins/query-params-controller';
 import { inject as injectService } from '@ember/service';
 import { readOnly } from '@ember/object/computed';
--- tests/dummy/app/routes/vr.js
+++ tests/dummy/app/routes/vr.js
@@ -1,4 +1,5 @@
 import Route from '@ember/routing/route';
+// eslint-disable-next-line ember/no-mixins
 import QueryParamsRouteMixin from 'ember-aframe-camera-extras/mixins/query-params-route';
 
 export default Route.extend(QueryParamsRouteMixin);
